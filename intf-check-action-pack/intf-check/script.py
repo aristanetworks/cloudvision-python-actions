bfb60caeb6f5818bb255a075afdc010cba0e016f
Cian McGrath <mcgrathc@arista.com>
2022-01-19 10:10:42 +0000

    actions: Add intf-check action pack
    Change-Id: I16ebdfc6635962a1bb8b2b361bea853f3461811b

---
+++ intf-check-action-pack/intf-check/script.py
@@ -0,0 +1,35 @@
+# Copyright (c) 2022 Arista Networks, Inc.
+# Use of this source code is governed by the Apache License 2.0
+# that can be found in the COPYING file.
+
+interfacesStr: str = ctx.changeControl.args.get("interfaces")
+# Split the comma separated string on the comma and strip of any whitespace
+interfacesList = [intf.strip() for intf in interfacesStr.split(',')]
+# Filter out empty values from the split
+interfaces = list(filter(None, interfacesList))
+# If the list is empty, raise an error
+if not interfaces:
+    raise UserWarning("Interface list passed to script is empty")
+
+ctx.alog("Running 'show interfaces status' to check if any important interfaces are down")
+cmds = [
+    "enable",
+    "show interfaces status",
+]
+cmdResponse = ctx.runDeviceCmds(cmds)
+interfaceStatuses = cmdResponse[1]['response'].get('interfaceStatuses')
+if not interfaceStatuses:
+    raise UserWarning(f"show interfaces status failed with: {cmdResponse[1]['error']}")
+
+down = []
+for interface in interfaces:
+    intfStatus: dict = interfaceStatuses[interface]
+    if intfStatus.get('lineProtocolStatus') != 'up' or intfStatus.get('linkStatus') != 'connected':
+        down.append(interface)
+
+# If any of the interfaces are down, report an error
+if down:
+    downStr = ', '.join([str(elem) for elem in down])
+    raise UserWarning(f"Interface(s) in down state: {downStr}")
+
+ctx.alog("All specified interfaces are up")
