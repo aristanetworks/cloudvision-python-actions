b0772218345b478e17c4acf9adfd2dec64d79d29
Cian McGrath <mcgrathc@arista.com>
2022-01-19 10:10:00 +0000

    actions: Add image preload action pack
    Change-Id: I591fccc4871f6b3d5348902e060593f12eef0cfe

---
+++ image-preload-action-pack/image-preload/script.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2022 Arista Networks, Inc.
+# Use of this source code is governed by the Apache License 2.0
+# that can be found in the COPYING file.
+
+# Note: This script will become obsolete once the image studio system
+# becomes available, after which this only serves as an example of script writing
+
+from urllib.parse import urljoin
+
+authority = ctx.changeControl.args.get("authority")
+path = ctx.changeControl.args.get("path")
+eos = ctx.changeControl.args.get("eos")
+vrf = ctx.changeControl.args.get("vrf")
+imageUrl = urljoin(f"https://{authority}", path + eos)
+
+ctx.alog(f"Downloading EOS image from {imageUrl}")
+cmds = [
+    "enable",
+    f"cli vrf {vrf}",
+    f"copy {imageUrl} flash:",
+]
+cmdResponses: list[dict] = ctx.runDeviceCmds(cmds)
+# Iterate through the list of responses for the commands, and if an error occurred in
+# any of the commands, raise an exception
+# Only consider the first error that is encountered as following commands require previous ones to succeed
+if next((err for resp in cmdResponses if (err := resp.get('error'))), None):
+    raise UserWarning(f"Preloading image failed with: {err}")
+ctx.alog("Downloading of Eos image completed successfully")
