f16ffee7b1ce36d1a8565b53607465c5dcd86dec
Cian McGrath <mcgrathc@arista.com>
2022-01-19 10:12:13 +0000

    actions: Add push-token action pack
    Change-Id: Ifba09eba4c1e7e14ae2cd47cb365a0b32195907e

---
+++ push-token-action-pack/push-token/script.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2022 Arista Networks, Inc.
+# Use of this source code is governed by the Apache License 2.0
+# that can be found in the COPYING file.
+
+from typing import List, Dict
+
+token = ctx.changeControl.args.get("token")
+filename = ctx.changeControl.args.get("filename")
+
+cmds = [
+    "enable",
+    {"cmd": f"copy terminal: file:{filename}", "input": token},
+]
+cmdResponses: List[Dict] = ctx.runDeviceCmds(cmds)
+# Iterate through the list of responses for the commands, and if an error occurred in
+# any of the commands, raise an exception
+# Only consider the first error that is encountered as following commands require previous ones to succeed
+errs = [resp.get('error') for resp in cmdResponses if resp.get('error')]
+if errs:
+    raise UserWarning(f"Copying of token to {filename} failed with: {errs[0]}")
