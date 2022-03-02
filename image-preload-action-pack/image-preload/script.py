88673266158ab489584dbadcdf56c3a3f05e8db2
Cian McGrath <mcgrathc@arista.com>
2022-03-02 09:56:02 +0000

    actions: Fix syntax issue with error parsing
    Error parsing being used is only compatible with a python
    version later than what CloudVision runs
    
    Change-Id: Ide83908368b3f5dc038a6983cdd2a648a063da4d

--- image-preload-action-pack/image-preload/script.py
+++ image-preload-action-pack/image-preload/script.py
@@ -23,6 +23,7 @@ cmdResponses: list[dict] = ctx.runDeviceCmds(cmds)
 # Iterate through the list of responses for the commands, and if an error occurred in
 # any of the commands, raise an exception
 # Only consider the first error that is encountered as following commands require previous ones to succeed
-if next((err for resp in cmdResponses if (err := resp.get('error'))), None):
-    raise UserWarning(f"Preloading image failed with: {err}")
+errs = [resp.get('error') for resp in cmdResponses if resp.get('error')]
+if errs:
+    raise UserWarning(f"Preloading image failed with: {errs[0]}")
 ctx.alog("Downloading of Eos image completed successfully")
--- push-token-action-pack/push-token/script.py
+++ push-token-action-pack/push-token/script.py
@@ -13,5 +13,6 @@ cmdResponses: list[dict] = ctx.runDeviceCmds(cmds)
 # Iterate through the list of responses for the commands, and if an error occurred in
 # any of the commands, raise an exception
 # Only consider the first error that is encountered as following commands require previous ones to succeed
-if next((err for resp in cmdResponses if (err := resp.get('error'))), None):
-    raise UserWarning(f"Copying of token to {filename} failed with: {err}")
+errs = [resp.get('error') for resp in cmdResponses if resp.get('error')]
+if errs:
+    raise UserWarning(f"Copying of token to {filename} failed with: {errs[0]}")
