bc523e7e309f2969b7c7138e38b2c2022bc107b9
Guillaume Vilar <guillaume.vilar@arista.com>
2022-04-28 11:37:19 +0100

    actions: Add create-self-signed-certificate action pack
    Change-Id: Ib389d4fff31544022d62c3b02805beb11517361b
    Signed-off-by: guillaume.vilar <guillaume.vilar@arista.com>

---
+++ create-self-signed-certificate-action-pack/create-self-signed-certificate/script.py
@@ -0,0 +1,59 @@
+# Copyright (c) 2022 Arista Networks, Inc.
+# Use of this source code is governed by the Apache License 2.0
+# that can be found in the COPYING file.
+
+# This action will generate a self-signed certificate and an associate key that can be used
+# in an SSL profile.
+# Example of configuration using the generated certificate and key on EOS:
+
+# management security
+#   ssl profile SSL_profile
+#     tls versions 1.2
+#     certificate self-signed.crt key self-signed.key
+# management api http-commands
+#   protocol https ssl profile SSL_profile
+
+
+# 1. Setup:
+device = ctx.getDevice()
+ctx.alog(f"device_id: [{device.id}] - ip: [{device.ip}] - hostname: [{device.hostName}]")
+cmdResponse = ctx.runDeviceCmds(["enable", "show hostname"])
+hostname = cmdResponse[1]['response']['hostname']
+fqdn = cmdResponse[1]['response']['fqdn']
+ctx.alog(f"Creating self-signed certificate for device with fqdn: {fqdn} - hostname: {hostname}")
+
+
+# 2. Commands creation:
+args = ctx.changeControl.args
+cmds = [
+    "enable",
+    f"security pki key generate rsa {args['key_length']} {args['key_file']}",
+
+    (f"security pki certificate generate self-signed {args['cert_file']} "
+        f"key {args['key_file']} "
+        f"validity {args['validity']} "
+        f"parameters common-name {hostname} "
+        f"country \"{args['country']}\" "
+        f"state \"{args['state']}\" "
+        f"locality \"{args['locality']}\" "
+        f"organization \"{args['organization']}\" "
+        f"organization-unit \"{args['organization_unit']}\" "
+        f"email {args['email']} "
+        f"subject-alternative-name dns {fqdn} email {args['email']} ip {device.ip}")
+]
+ctx.alog(f"Command to run on the device: {cmds}")
+
+
+# 3. Run the commands on the device and check for errors:
+output_cmd_list = ctx.runDeviceCmds(cmds)
+ctx.alog(f"Outputs: {output_cmd_list}")
+for index, cmdOutput in enumerate(output_cmd_list):
+    if 'error' in cmdOutput.keys() and cmdOutput['error'] != '':
+        raise UserWarning(f"Error: switch {fqdn} - Command: '{cmds[index]}' \n Error: {cmdOutput}")
+
+
+# 4. Verification step:
+cert_dir_output = ctx.runDeviceCmds(["enable", "dir certificate:"])
+key_dir_output = ctx.runDeviceCmds(["enable", "dir sslkey:"])
+ctx.alog(f"Verification - Certificate directory: {cert_dir_output[1]['response']}")
+ctx.alog(f"Verification - Key directory: {key_dir_output[1]['response']}")
