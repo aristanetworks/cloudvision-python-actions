37b94d0f9ce564975a81856f6fe4c30287c95ba6
Cian McGrath <mcgrathc@arista.com>
2025-10-13 10:30:51 +0100

    tac-bundle-action: Fix EOS version determination
    We currently determine the version compatible with the support bundle
    incorrectly, as it considers patch version 0 of all releases after 26
    to be incompatible with the command, when they should actually be
    compatible.
    
    Also perform some minor cleanup and run the ruff formatter on the action
    
    Closes: Bug1290288
    Change-Id: Ia1b6332ac30c0e55bc1fdb148561548bab3f1f10

--- src/tac-bundle-action-pack/tac-bundle/script.py
+++ src/tac-bundle-action-pack/tac-bundle/script.py
@@ -2,16 +2,17 @@
 # Use of this source code is governed by the Apache License 2.0
 # that can be found in the COPYING file.
 import re
+
 dt = "$HOSTNAME-$(date +%Y_%m_%d.%H_%M_%S)"
 srn: str = ctx.action.args.get("SRNumber")
 
 cmd = ["show version"]
-version = ctx.runDeviceCmds(cmd)[0]['response']['version']
+version = ctx.runDeviceCmds(cmd)[0]["response"]["version"]
 ctx.info(str(version))
-vl = version.split('.')
-regex_new_version = re.match(r'([0-9]{1,2}).*', vl[2]).group(1)
+vl = version.split(".")
+regex_new_version = re.match(r"([0-9]{1,2}).*", vl[2]).group(1)
 # If EOS is higher than 4.26.1F+ we can run the new support-bundle command
-if int(vl[1]) >= 26 and int(regex_new_version) >= 1:
+if (int(vl[1]) > 26) or (int(vl[1]) == 26 and int(regex_new_version) >= 1):
     if not srn:
         baseline = ["send support-bundle flash:"]
     else:
@@ -25,26 +26,33 @@ else:
         prefix = "TAC-" + srn
         tar_prefix = "-SR" + srn
     baseline = [
-        ("bash timeout 10 sudo tar -cvf - /mnt/flash/schedule/tech-support/* "
-         f"> /mnt/flash/{prefix}-hist-{dt}.tar"),
+        (
+            "bash timeout 600 sudo tar -cvf - /mnt/flash/schedule/tech-support/* "
+            f"> /mnt/flash/{prefix}-hist-{dt}.tar"
+        ),
         f"show tech-support | gzip > /mnt/flash/{prefix}-show-tech-{dt}.log.gz",
         f"show agent logs | gzip > /mnt/flash/{prefix}-show-agentlog-{dt}.log.gz",
         f"bash sudo tar -czvf - /var/log/qt/ > /mnt/flash/{prefix}-qt-logs-{dt}.tar.gz",
         f"show aaa accounting logs | gzip >/mnt/flash/{prefix}-show-aaa-accounting-{dt}.log.gz",
-        f"show logging system | gzip >/mnt/flash/{prefix}-show-logsys-{dt}.log.gz"
+        f"show logging system | gzip >/mnt/flash/{prefix}-show-logsys-{dt}.log.gz",
     ]
     if int(vl[1]) >= 21 and int(regex_new_version) >= 0:
-        baseline.append(
-            f"show tech extended evpn | gzip > /mnt/flash/{prefix}-show-tech-evpn-{dt}.log.gz")
-        baseline.append(
-            f"show arp vrf all | gzip > /mnt/flash/{prefix}-show-arp-vrf-all-{dt}.log.gz")
+        baseline.extend(
+            [
+                f"show tech extended evpn | gzip > /mnt/flash/{prefix}-show-tech-evpn-{dt}.log.gz",
+                f"show arp vrf all | gzip > /mnt/flash/{prefix}-show-arp-vrf-all-{dt}.log.gz",
+            ]
+        )
     baseline.append(
-        ("bash timeout 8 sudo tar -cvf - /mnt/flash/TAC-* "
-         f"> /mnt/flash/support-bundle{tar_prefix}-{dt}.tar"))
+        (
+            "bash timeout 600 sudo tar -cvf - /mnt/flash/TAC-* "
+            f"> /mnt/flash/support-bundle{tar_prefix}-{dt}.tar"
+        )
+    )
 ctx.info(f"Gathering baseline logs from device {ctx.getDevice().ip}")
 ctx.runDeviceCmds(baseline, fmt="text", timeout=800, diConnTimeout=600, diCliTimeout=600)
 check_files = ["dir /all flash:"]
 ctx.info(f"Listing the content of flash on device {ctx.getDevice().ip}")
 result = ctx.runDeviceCmds(check_files)
-ctx.info(str(result[0]['response']))
+ctx.info(str(result[0]["response"]))
 ctx.info("Please upload the support-bundle on the switch(es) flash to the TAC case.")
