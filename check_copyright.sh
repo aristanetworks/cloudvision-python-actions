54f6b586a7558222e0c57f4b079ec12d6663d5d1
Roman Statkevych <rstatkevych@arista.com>
2025-11-12 11:08:44 +0000

    check_copyright: Fix copyright check
    Previously it was only comparing unstaged changes, which doesn't work
    if this changes are commited and are running through checks in CI
    pipelines.
    
    Also, fix dubious ownership issue on Jenkins
    
    Change-Id: I900757c6f391d49c05a389028744f8bcd5e4ee2a

--- check_copyright.sh
+++ check_copyright.sh
@@ -1,7 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 # Copyright (c) 2022 Arista Networks, Inc.
 # Use of this source code is governed by the Apache License 2.0
 # that can be found in the COPYING file.
+
+# This script checks if all the changed files in current revision
+# have proper copyright headers
+#
+# Usage: ./check_copyright.sh [<branch>]
+#
+# 	branch is optional - you can provide base branch manually (e.g `trunk`)
+#	otherwise it would use `git log --first-parent`
 
 # egrep that comes with our Linux distro doesn't like \d, so use [0-9]
 notice='Copyright \(c\) 20[0-9][0-9] Arista Networks, Inc.'
@@ -9,7 +17,22 @@ apacheNotice="Use of this source code is governed by the Apache License 2.0"
 apacheNoticeSource="that can be found in the COPYING file."
 confidential="Confidential and Proprietary"
 
-files=`git diff-tree --no-commit-id --name-only --diff-filter=ACMR -r HEAD | \
+# To omit dubious ownership git errors
+repoFolder=`dirname "${BASH_SOURCE[0]}"`
+cd $repoFolder && git config --global --add safe.directory $repoFolder
+
+if [ ! -z "$1" ]; then
+	branchBaseCommit=`git show $1 --format=format:%H --no-patch`
+	exitCode=$?
+	if [ $exitCode != "0" ]; then
+		echo Error finding revision $1
+		exit $exitCode
+	fi
+else
+	branchBaseCommit=`git show --first-parent --format=format:%H --no-patch`
+fi
+
+files=`git diff-tree --no-commit-id --name-only --diff-filter=ACMR -r $branchBaseCommit | \
 	egrep '\.(py|sh)$'`
 status=0
 
